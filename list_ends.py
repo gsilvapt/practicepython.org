30a20ebdcecf17124b1ace02a061793ec068976b
gsilvapt <gustavosantaremsilva@gmail.com>
2017-08-28 12:59:45 +0100

    Fix styling and improve code readability

--- list_ends.py
+++ list_ends.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env/python3
 import random
 
-def getList(prompt):
+def get_list(prompt):
     """
     by: gsilvapt
     Function to get a list from user input or generate a random one.
     (str) -> list, int
     """
-    return input(prompt)
-
+    return random.sample(range(1, 101), random.randint(1, 101))
+    
 def first_and_last(a_list):
     """
     by: gsilvapt
@@ -16,9 +16,9 @@ def first_and_last(a_list):
     randomly or given by user.
     (lst) -> int, int
     """
-    a_list = random.sample(range(1, 101), random.randint(1, 101))
+    a_list = get_list(a_list)
     print([a_list[0], a_list[len(a_list)-1]])
 
 if __name__ == "__main__":
-    first_and_last(getList('Press any key to generate a random list and' \
+    first_and_last(get_list('Press any key to generate a random list and' \
             ' find its first and last values. '))
