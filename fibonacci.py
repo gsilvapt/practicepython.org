db01095b2d32b7784ab159de074e84b2c616c118
gsilvapt <gustavosantaremsilva@gmail.com>
2017-08-31 00:23:39 +0100

    Styling fixes

--- fibonacci.py
+++ fibonacci.py
@@ -5,13 +5,17 @@ def get_intervals(prompt):
     by: gsilvapt
     Function to retrieve information on the number of fibonacci numbers user
     wants to generate.
-    ex: How many fibonacci numbers you want to generate. 
+    ex: How many fibonacci numbers you want to generate.
     sequence.
     (int) -> list, int
     '''
     return input(prompt)
 
 def fib_seq(numbers):
+    '''
+    Function to compute fibonacci numbers.
+    (int) -> int
+    '''
     fib = []
     i = 1
     if int(numbers) == 0:
@@ -25,8 +29,8 @@ def fib_seq(numbers):
         while i < (int(numbers)- i):
             fib.append(fib[i] + fib[i-1])
             i += 1
-            
-    print(fib) 
+    print(fib)
+
 if __name__ == "__main__":
     fib_seq(get_intervals('How many fibonacci numbers do you wish to '\
             'generate? '))
